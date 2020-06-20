f61d266751ef66e97e9d548f1af02e029716c3bd
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-06-20 14:24:24 +0300

    exceptions

--- src/srv.py
+++ src/srv.py
@@ -5,9 +5,25 @@ from http.server import SimpleHTTPRequestHandler
 from typing import Dict
 from typing import Union
 from urllib.parse import parse_qs
+from pathlib import Path
 
 PORT = int(os.getenv("PORT", 8000))
 print(f"PORT = {PORT}")
+
+
+PROJECT_DIR = Path(__file__).parent.parent.resolve()
+print(f"PROJECT_DIR = {PROJECT_DIR}")
+
+TEMPLATES_DIR = PROJECT_DIR / "templates"
+print(f"TEMPLATES_DIR = {TEMPLATES_DIR}")
+
+
+class NotFound(Exception):
+    pass
+
+
+class MethodNotAllowed(Exception):
+    ...
 
 
 class MyHandler(SimpleHTTPRequestHandler):
@@ -16,14 +32,42 @@ class MyHandler(SimpleHTTPRequestHandler):
         handlers = {
             "/hello": self.handle_hello,
             "/goodbye": self.handle_goodbye,
+            "/projects": self.handle_projects,
+            "/resume": self.handle_resume,
         }
 
         default_handler = super().do_GET
 
         handler = handlers.get(path, default_handler)
-        handler()
+        try:
+            handler()
+        except NotFound:
+            self.respond_404()
+        except MethodNotAllowed:
+            self.respond_405()
+
+    def handle_projects(self):
+        html = TEMPLATES_DIR / "projects" / "index.html"
+        contents = self.get_file_contents(html)
+        self.respond(contents, "text/html")
+
+    def handle_resume(self):
+        html = TEMPLATES_DIR / "resume" / "index.html"
+        contents = self.get_file_contents(html)
+        self.respond(contents, "text/html")
+
+    def get_file_contents(self, fp: Path) -> str:
+        if not fp.is_file():
+            raise NotFound
+
+        with fp.open("r") as src:
+            ct = src.read()
+        
+        return ct
 
     def handle_hello(self) -> None:
+        raise MethodNotAllowed
+
         args = self.build_query_args()
         name = self.build_name(args)
         age = self.build_age(args)
@@ -42,7 +86,6 @@ class MyHandler(SimpleHTTPRequestHandler):
         self.respond(msg)
 
     def build_query_args(self) -> Dict:
-        breakpoint()
         _path, *qs = self.path.split("?")
         args = {}
 
@@ -75,8 +118,26 @@ class MyHandler(SimpleHTTPRequestHandler):
             return age
         return int(age)
 
-    def respond(self, msg: str) -> None:
+    def respond(self, msg: str, content_type="text/plain") -> None:
         self.send_response(200)
+        self.send_header("Content-type", content_type)
+        self.send_header("Content-length", str(len(msg)))
+        self.end_headers()
+
+        self.wfile.write(msg.encode())
+
+    def respond_404(self) -> None:
+        msg = "Ooops! Nothing found!"
+        self.send_response(404)
+        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-length", str(len(msg)))
+        self.end_headers()
+
+        self.wfile.write(msg.encode())
+
+    def respond_405(self) -> None:
+        msg = "Method Not Allowed"
+        self.send_response(405)
         self.send_header("Content-type", "text/plain")
         self.send_header("Content-length", str(len(msg)))
         self.end_headers()
