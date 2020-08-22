6f794749d48b4f95e034bf427e844d907bca0890
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-08-22 13:47:49 +0300

    Added truncatesessions command (#6)
    * configured dynaconf via dotenv
    
    * Lesson 22: dynaconf
    
    * refactored
    
    * black
    
    * Added truncatesessions command

---
+++ src/applications/target/management/commands/truncatesessions.py
@@ -0,0 +1,9 @@
+from django.contrib.sessions.models import Session
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Truncates Django session table'
+
+    def handle(self, *args, **options):
+        Session.objects.all().delete()
