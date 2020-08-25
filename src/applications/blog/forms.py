82c5e4c2aa43f26a8d23213279472310bf20545d
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-08-25 22:03:41 +0300

    fixed typo in label

--- src/applications/blog/forms.py
+++ src/applications/blog/forms.py
@@ -5,9 +5,9 @@ from applications.blog.models import Tweet
 
 class TweetForm(forms.ModelForm):
     class Meta:
-        model = Tweet
         fields = [Tweet.content.field.name]
+        labels = {Tweet.content.field.name: "What's on your mind?"}
+        model = Tweet
         widgets = {
             Tweet.content.field.name: forms.Textarea(attrs={"cols": 70, "rows": 2}),
         }
-        labels = {Tweet.content.field.name: "SayView something:"}
