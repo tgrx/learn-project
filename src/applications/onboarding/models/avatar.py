dc69b8c35bff5c8f85664b3aa17296e56ef7afde
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-09-05 12:55:28 +0300

    FIX: custom s3 location for env/user

--- src/applications/onboarding/models/avatar.py
+++ src/applications/onboarding/models/avatar.py
@@ -1,12 +1,19 @@
+from django.conf import settings
 from django.db import models
 from storages.backends.s3boto3 import S3Boto3Storage
+
+
+def upload_to(instance: "Avatar", filename):
+    return f"{settings.AWS_S3_AVATARS_LOCATION}/avatar__{instance.pk}__{filename}"
 
 
 class Avatar(models.Model):
     profile = models.OneToOneField(
         "Profile", on_delete=models.CASCADE, primary_key=True
     )
-    original = models.FileField(storage=S3Boto3Storage(), upload_to="avatars")
+    original = models.FileField(
+        storage=S3Boto3Storage(), upload_to=upload_to, null=True, blank=True
+    )
 
     def __str__(self):
         msg = f"{self.profile}"
